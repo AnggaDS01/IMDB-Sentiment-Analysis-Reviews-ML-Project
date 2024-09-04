dcdcb176e072222881c8f6ab7a0f1b3363a7c688
Angga Dwi Sunarto <kanshinbatosai@gmail.com>
2024-09-04 18:33:17 +0700

    menambahkan logger dan custom exception

---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+from setuptools import find_packages, setup
+
+setup(
+    name='IMDB-Sentiment-Analysis-Reviews-ML-Project',
+    version='0.0.1',
+    author='Angga Dwi Sunarto',
+    author_email='anggadwisunarto3@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
