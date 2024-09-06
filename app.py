05399d49a9794b251988a1ec733fc8ce62d97362
Angga Dwi Sunarto <kanshinbatosai@gmail.com>
2024-09-06 15:33:29 +0700

    menambahkan data transformation pipeline

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from imbd_sentiment.pipeline.train_pipeline import TrainPipeline
 
 obj = TrainPipeline()
-data_ingestion_artifacts = obj.run_pipeline()
-print(data_ingestion_artifacts)
+obj.run_pipeline()
